b98a80735153b6d3679ca2b5cd571d6a56ded5a6
Connor <39162958+bigc0127@users.noreply.github.com>
2020-09-09 10:13:19 -0400

    Update Auto-install-bashrc.sh

--- Auto-install-bashrc.sh
+++ Auto-install-bashrc.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# clone pureline-pinephone
-git clone https://github.com/bigc0127/pureline-pinephone.git
-# install pureline config
+# install pureline conf
 cp pureline-pinephone/configs/powerline_full_256col.conf ~/.pureline.conf
 # install fonts and append bash
 git clone https://github.com/powerline/fonts.git --depth=1
